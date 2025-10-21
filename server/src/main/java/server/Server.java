574a15b13c87e362b3ec9510a24a8067365068a4
Daniel-Layton <daniellayton02@gmail.com>
2025-10-21 15:50:42 -0600

    Created Register Handler and hit it from the website

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
 
 public class Server {
 
@@ -8,9 +9,13 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
         // Register your endpoints and exception handlers here.
+        javalin.post("/user", this::RegisterHandler);
+    }
 
+    private void RegisterHandler(Context ctx) {
+        System.out.println("Register Handler Hit!");
+        ctx.status(400);
     }
 
     public int run(int desiredPort) {
